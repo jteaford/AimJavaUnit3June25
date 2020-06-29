79a74e4b3b995a0ef2e8082ce5b5dbb16fc30eb8
jteaford <jessteaford@gmail.com>
2020-06-29 10:00:17 -0500

    Initial Commit

--- src/exercise4/AimJavaExercise4.java
+++ src/exercise4/AimJavaExercise4.java
@@ -35,13 +35,14 @@ public class AimJavaExercise4 {
         System.out.print(y2);
         System.out.print(")");
         System.out.print(" is ");
-        System.out.println(p1.distance(x2,y2));
+        System.out.format("%1.2f", p1.distance(x2,y2));
+        System.out.println("");
 
         // Uses the parameterized constructor to create a MyPoint object p2 using the x coordinate and y coodinate entered by the user.
         MyPoint p2 = new MyPoint(x2, y2);
 
         // Uses the second and third distance method to calculate and display the distance between the MyPoint object p1 and the MyPoint object p2. The distance is displayed with two digits after the decimal point.
-        System.out.print("Using method 2, the distance from (");
+        System.out.println("Using method 2, the distance from (");
         System.out.print(p1.getX());
         System.out.print(",");
         System.out.print(p1.getY());
@@ -52,23 +53,22 @@ public class AimJavaExercise4 {
         System.out.print(p2.getY());
         System.out.print(")");
         System.out.print(" is ");
-        System.out.println(p1.distance(p2));
+        System.out.format("%1.2f", p1.distance(p2));
+        System.out.println("");
 
-        MyPoint mp1 = new MyPoint();
-        MyPoint mp2 = new MyPoint();
-
-        System.out.print("Using method 3, the distance from (");
-        System.out.print(mp1.getX());
+        System.out.println("Using method 3, the distance from (");
+        System.out.print(p1.getX());
         System.out.print(",");
-        System.out.print(mp1.getY());
+        System.out.print(p1.getY());
         System.out.print(")");
         System.out.print(" to (");
-        System.out.print(mp2.getX());
+        System.out.print(p2.getX());
         System.out.print(",");
-        System.out.print(mp2.getY());
+        System.out.print(p2.getY());
         System.out.print(")");
         System.out.print(" is ");
-        System.out.println(MyPoint.distance(mp1, mp2));
+        System.out.format("%1.2f", MyPoint.distance(p1, p2));
+        System.out.println("");
 
         // Displays a goodbye message.
         System.out.println("Goodbye ...");
