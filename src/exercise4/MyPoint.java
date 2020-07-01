6b94e168247358210cdfcf4240e22ecf3a1e687b
jteaford <jessteaford@gmail.com>
2020-06-30 19:23:35 -0500

    Initial Commit

--- src/exercise4/MyPoint.java
+++ src/exercise4/MyPoint.java
@@ -2,8 +2,8 @@ package exercise4;
 
 public class MyPoint {
     // Two data fields x and y, that represent the x-coordinate and the y-coordinate of the point.
-    int x;
-    int y;
+    private int x;
+    private int y;
 
     // Getter and setter methods for x and y.
     public int getX() {
@@ -23,18 +23,20 @@ public class MyPoint {
     }
 
     // A no-arg constructor that creates a default point at (0,0).
-    public MyPoint() {
+    MyPoint() {
         x = 0;
         y = 0;
         }
 
     // A parameterized constructor that creates a point at the designated x and y coordinate.
-    public MyPoint(int x, int y) {
-    this.x = x;
-    this.y = y;
+    MyPoint(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 
-    // A member method named distance that calculates and returns the distance between this MyPoint object and another point that is specified by its x- and y-coordinates. The method header looks like this: public double distance(int xCoord, int yCoord);
+    // A member method named distance that calculates and returns the distance between this MyPoint 
+    // object and another point that is specified by its x- and y-coordinates. The method header looks like this: 
+    // public double distance(int xCoord, int yCoord);
         public double distance(int xCoord, int yCoord){
             int x1 = this.getX();
             int y1 = this.getY();
@@ -44,7 +46,8 @@ public class MyPoint {
             return Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
         }
 
-    // A member method named distance that calculates and returns the distance between this MyPoint object and another object of the MyPoint class. The method header looks like this: public double distance(MyPoint p)
+    // A member method named distance that calculates and returns the distance between this MyPoint object and another 
+    // object of the MyPoint class. The method header looks like this: public double distance(MyPoint p)
         public double distance(MyPoint p){
             int x1 = this.getX();
             int y1 = this.getY();
@@ -54,7 +57,9 @@ public class MyPoint {
             return Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
         }
 
-    // A static, non-member method named distance that takes two objects of the MyPoint class as parameters, and calculates and returns the distance between the two of them. The method header looks like this: public static double distance (MyPoint mp1, MyPoint mp2)
+    // A static, non-member method named distance that takes two objects of the MyPoint class as parameters, and 
+    // calculates and returns the distance between the two of them. The method header looks like this: 
+    // public static double distance (MyPoint mp1, MyPoint mp2)
         public static double distance(MyPoint mp1, MyPoint mp2){
             int x1 = mp1.getX();
             int y1 = mp1.getY();
